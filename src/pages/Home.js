2336cd183b4cd05ad4e834c58369d994b811ab8f
Arjunagi A Rehman <abdul123arj@gmail.com>
2024-02-11 21:26:24 +0530

    change home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,17 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <section id="Home" className="container text-dark Home-page">
+    <div className="text-center">
+      <h1>AnchorSub</h1>
+    </div>
+    <br/><br/>
+    <p>
+      Earn and Achive
+    </p>
+    <div className="text-center">
+      <button type="button" className="btn btn-primary"><a className="text-white" href="/Generate">Generate</a></button>
+    </div>
+  </section>  
   )
 }
